9948c24509d2505180fd67614809231cab9ea772
ashiqurras <ashiqurrussel2@gmail.com>
2021-12-13 14:36:55 +0100

    use state basic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import SetUp from './tutorial/1-useState/setup/2-useState-basics'
+
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <SetUp />
     </div>
   )
 }
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+ 
+  let title = "random title";
+  const [text,setText]=useState(title);
+
+  const handleClick=()=>{
+
+    if(text===title){
+            setText("Hello World");
+    }else{
+      setText(title);
+    }
+  }
+  return( 
+    <React.Fragment>
+    <h1>{text}</h1>
+    <button type="button" className="btn" onClick={handleClick}>Change Title</button>
+    </React.Fragment>
+    );
 };
 
 export default UseStateBasics;
