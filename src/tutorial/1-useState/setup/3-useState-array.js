af9e5e82c4a65309da85351e7a574167e147abe7
ashiqurras <ashiqurrussel2@gmail.com>
2021-12-13 23:31:46 +0100

    delete data on useState

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import { data } from '../../../data';
 
+import { useState } from 'react';
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+
+  const [people,setPeople]=useState(data)
+  // function for delete individual item by id
+  const removeItem = (id) => {
+          // filter individual item 
+          let newPeople = people.filter((person) => person.id !== id);
+          setPeople(newPeople);
+  }
+
+
+  return (
+    <>
+      {
+        people.map((person)=>{
+          const {id,name}=person;
+          return (
+            <div key={id} className='item'>
+              <h4>{name}</h4>
+              <button onClick={() => removeItem(id)}>Remove</button>
+            </div>
+          )
+        })
+      }
+
+      <button type="button" className="btn" onClick={()=>setPeople([])}>Delete</button>
+
+    </>
+  )
 };
 
 export default UseStateArray;
