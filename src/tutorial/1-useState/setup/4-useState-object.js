ae03b4099f3fcabb26e02465d3510ae5d5fffcd4
ashiqurras <ashiqurrussel2@gmail.com>
2021-12-14 03:15:19 +0100

    Normal Counter App

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,28 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Ashiqur Russel",
+    age: 28,
+    message: 'Happy Birth Day!'
+  })
+
+  const changeMessage =()=>{
+    setPerson({...person,message:'Wish you good luck!'})
+  }
+  return (
+
+    <>
+    <div>
+      <h3 className='item'>{person.name}</h3>
+      <h3 className='item'>{person.age}</h3>
+      <h3 className='item'>{person.message}</h3>
+      <button className='btn' onClick={changeMessage}>Change Message</button>
+
+    </div>
+    
+    </>
+  );
 };
 
 export default UseStateObject;
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,37 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+
+  const [counter,changeCounter] = useState(0);
+
+  //Handler for Increase value
+  const btnIncrease = ()=>{
+    changeCounter(counter+1);
+  }
+
+  //Handler for Decrease value
+  const btnDecrease = ()=>{
+        changeCounter(counter-1);
+  }
+  
+  //Handler for Reseting value
+  const btnReset = ()=>{
+    changeCounter(0);
+  }
+  
+  
+  return(
+    <>
+    <section style={{margin:'4rem 0'}}>
+      <h1>Regular Counter</h1>
+      <h1>{counter}</h1>
+      <button className='btn' onClick={btnIncrease}>Increse</button>
+      <button className='btn'onClick={btnDecrease}>Decrease</button>
+      <button className='btn' onClick={btnReset}>Reset</button>
+
+    </section>
+    </>
+  )
 };
 
 export default UseStateCounter;
