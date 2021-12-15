87b2d176624bdccb8b8b0821e786cc3c542145c4
ashiqurras <ashiqurrussel2@gmail.com>
2021-12-15 01:42:25 +0100

    fetch github user from API using useEffect hook and async function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 //import SetUp from './tutorial/1-useState/setup/5-useState-counter'
 //import SetUp from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
-import Dog from './tutorial/2-useEffect/setup/DogImage'
+import User from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
 
 
 function App() {
   return (
     <div className='container'>
-      <Dog />
+      <User />
     </div>
   )
 }
